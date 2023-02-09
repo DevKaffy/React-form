3a50311b28f5e32d4eed60e0de1b4723b5c3d462
Kafilat <kafilatadewumi61@gmail.com>
2023-02-09 08:53:14 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,25 @@ setLists("");
   
   return (
     <div className="App">
-      <h1 className='mb-[1rem] text-3xl'>Add Note</h1>
+      <h1 className="mb-[1rem] text-3xl">Add Note</h1>
       <form onSubmit={handleSubmit}>
-        <input className='px-4 py-2 mb-[1rem]'
+        <input
+          className="px-4 py-2 mb-[1rem]"
           type="text"
           value={lists}
           onChange={(e) => setLists(e.target.value)}
         />
         <br />
-        <button className='bg-[blue] text-[white] mb-[1rem] px-6 py-2 rounded-full' type='submit'>
+        <button
+          className="bg-[blue] text-[white] mb-[1rem] px-6 py-2 rounded-full"
+          type="submit"
+        >
           Add
         </button>
       </form>
-      <div className='bg-[red] w-[20rem] h-[20rem] ml-[10rem] px-[1rem] py-[1rem]'>
+      <div className="bg-[blue] w-[20rem] h-[20rem] ml-[10rem] px-[1rem] py-[1rem]">
         {notes.map((note, index) => (
-            <li>{note.lists}</li>
+          <li>{note.lists}</li>
         ))}
       </div>
     </div>
