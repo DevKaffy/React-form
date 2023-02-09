26296ef828f48f418d4a3048915d0553638d91d9
Kafilat <kafilatadewumi61@gmail.com>
2023-02-09 08:47:56 +0100

    feat: README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# React Form
+React form, adding notes to a form.
+
+## Prequisite
+- Your browser version should be up to date for better experience
+
+## Features
+- Home page
+
+## Tech/framework used
+- Reactjs
+- Javascript
+- TailwindCss
+
+## Preview here
+[Go Live](https://https://reactform-addnote.netlify.app/)
+
+
+## Contact
+If you want to contact me you can reach me at
+- https://github.com/DevKaffy or
+- kafilatadewumi61@gmail.com
+
+## Addtional Info
+- This is not meant for production. It's for learning purpose only
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const initialNote =['Adewumi, Kafilat Eniola']
-  
-  const [count, setCount] = useState(0)
+  const initialNote =['Hello']
+  const [lists, setLists] = useState("")
+  const [notes, setNotes] = useState(initialNote);
+
+  const handleSubmit= (e)=> {
+e.preventDefault();
+setNotes(notes.concat({lists}))
+setLists("");
+  }
   
   return (
     <div className="App">
-      
+      <h1 className='mb-[1rem] text-3xl'>Add Note</h1>
+      <form onSubmit={handleSubmit}>
+        <input className='px-4 py-2 mb-[1rem]'
+          type="text"
+          value={lists}
+          onChange={(e) => setLists(e.target.value)}
+        />
+        <br />
+        <button className='bg-[blue] text-[white] mb-[1rem] px-6 py-2 rounded-full' type='submit'>
+          Add
+        </button>
+      </form>
+      <div className='bg-[red] w-[20rem] h-[20rem] ml-[10rem] px-[1rem] py-[1rem]'>
+        {notes.map((note, index) => (
+            <li>{note.lists}</li>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
